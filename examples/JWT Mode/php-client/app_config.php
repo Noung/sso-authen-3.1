2578e6846c276607ae758ad1dfe00b42ba49d191
Kittisak Kaewneam <kittisak84@gmail.com>
2025-09-19 11:39:43 +0700

    Update app_config.php

--- examples/JWT Mode/php-client/app_config.php
+++ examples/JWT Mode/php-client/app_config.php
@@ -3,4 +3,4 @@
 
 // **สำคัญมาก:** กุญแจลับนี้ต้องตรงกับค่า JWT_SECRET_KEY
 // ในไฟล์ config.php ของ sso-authen ทุกประการ
-define('JWT_SHARED_SECRET_KEY', 'YOUR_SUPER_SECRET_KEY_FOR_JWT_GENERATION_THAT_IS_VERY_LONG');
+define('JWT_SECRET_KEY', 'YOUR_SUPER_SECRET_KEY_FOR_JWT_GENERATION_THAT_IS_VERY_LONG');
